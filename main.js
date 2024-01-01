1ce34b9b8805006999f507e156e518afe92ed518
drat1n1 <125225830+drat1n1@users.noreply.github.com>
2024-01-02 02:22:29 +0900

    [add]
    1

---
+++ main.js
@@ -0,0 +1,5 @@
+let inputBox = document.getElementById("input");
+let addToDo = document.getElementById("addToDo");
+let toDoList = document.getElementById("toDoList");
+
+addToDo.addEventListener("click", function () {});
